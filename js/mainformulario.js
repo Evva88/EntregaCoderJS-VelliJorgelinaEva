29e55b0e58dfae268c835ddcfbf8203edad52a4c
unknown <evavelli5@gmail.com>
2023-04-16 07:28:30 -0300

    form

--- js/mainformulario.js
+++ js/mainformulario.js
@@ -1,34 +1,39 @@
 class Cliente {
-     constructor(nombre,apellido,mail){
-     this.nombre = nombre;
-     this.apellido = apellido;
-     this.mail = mail;
+     constructor(nombre, apellido, mail) {
+       this.nombre = nombre;
+       this.apellido = apellido;
+       this.mail = mail;
      }
-}
-
-const arrayClientes = [];
-
-
-const formulario = document.getElementById('formulario');   
-formulario.addEventListener("submit", (e)=>{
-
+   }
+   
+   const arrayClientes = [];
+   
+   const formulario = document.getElementById("formulario");
+   formulario.addEventListener("submit", (e) => {
      e.preventDefault();
- 
+   
      const nombre = document.getElementById("nombre");
-     const apellido = document.getElementById ("apellido");
-     const mail = document.getElementById ("mail");
- 
-     const cliente = new Cliente (nombre.value, apellido.value, mail.value);
- 
+     const apellido = document.getElementById("apellido");
+     const mail = document.getElementById("mail");
+   
+     // Validación de formulario
+     if (nombre.value === "" || apellido.value === "" || mail.value === "") {
+       alert("Por favor complete todos los campos del formulario");
+       return;
+     }
+   
+     const cliente = new Cliente(nombre.value, apellido.value, mail.value);
+   
      arrayClientes.push(cliente);
      console.log(arrayClientes);
- 
+   
+     // Almacenar los datos en localStorage
+     localStorage.setItem("arrayClientes", JSON.stringify(arrayClientes));
+   
      formulario.reset();
-
-     console.log(arrayClientes);
-
-localStorage.setItem("arrayClientes", JSON.stringify(arrayClientes));
-let arrayClienteAgregado = JSON.parse(localStorage.getItem("arrayClientes"));
-
-arrayClienteAgregado();
-})
+   });
+   
+   // Obtener los datos del localStorage y asignarlos a la variable arrayClienteAgregado
+   let arrayClienteAgregado = JSON.parse(localStorage.getItem("arrayClientes"));
+   console.log(arrayClienteAgregado);
+   
